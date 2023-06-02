a66b1e24a62d00a22e9652b221677307d36a0c11
velopace <velopace@outlook.com>
2023-06-02 12:43:01 -0400

    migrations and seeding

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -4,8 +4,9 @@ namespace PokemonReviewApp.Models
 	public class Owner
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Gym { get; set; }
+		public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gym { get; set; }
 		public Country Country { get; set; }
 		public ICollection<PokemonOwner> PokemonOwners { get; set; }
 	}
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -11,6 +11,7 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        builder.Services.AddTransient<Seed>();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -20,6 +21,20 @@ public class Program
         });
 
         var app = builder.Build();
+
+        if (args.Length == 1 && args[0].ToLower() == "seeddata")
+            SeedData(app);
+
+        void SeedData(IHost app)
+        {
+            var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopedFactory.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetService<Seed>();
+                service.SeedDataContext();
+            }
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
