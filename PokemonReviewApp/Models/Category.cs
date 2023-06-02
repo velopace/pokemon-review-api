ac86fc3e0bcdd3f19cdef5950439a15a529a303f
velopace <velopace@outlook.com>
2023-06-02 10:24:28 -0400

    many to many rels

--- PokemonReviewApp/Models/Category.cs
+++ PokemonReviewApp/Models/Category.cs
@@ -5,6 +5,7 @@ namespace PokemonReviewApp.Models
 	{
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
 
--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -7,6 +7,7 @@ namespace PokemonReviewApp.Models
 		public string Name { get; set; }
 		public string Gym { get; set; }
 		public Country Country { get; set; }
+		public ICollection<PokemonOwner> PokemonOwners { get; set; }
 	}
 }
 
