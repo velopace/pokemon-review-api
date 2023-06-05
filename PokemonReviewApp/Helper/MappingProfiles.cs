3f212643dee4be6b42b8d402ceab348237332d77
velopace <velopace@outlook.com>
2023-06-05 14:27:28 -0400

    add country and category controller

--- PokemonReviewApp/Helper/MappingProfiles.cs
+++ PokemonReviewApp/Helper/MappingProfiles.cs
@@ -10,6 +10,8 @@ namespace PokemonReviewApp.Helper
 		public MappingProfiles()
 		{
             CreateMap<Pokemon, PokemonDto>();
+			CreateMap<Category, CategoryDto>();
+			CreateMap<Country, CountryDto>();
         }
 	}
 }
