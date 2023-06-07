123c81dabe549adcf517bef27d6dde1b9c69f801
velopace <velopace@outlook.com>
2023-06-07 07:09:33 -0400

    update category

--- PokemonReviewApp/Repository/CategoryRepository.cs
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -44,6 +44,12 @@ namespace PokemonReviewApp.Repository
             var saved = _context.SaveChanges();
             return saved > 0 ? true : false;
         }
+
+        public bool UpdateCategory(Category category)
+        {
+            _context.Update(category);
+            return Save();
+        }
     }
 }
 
