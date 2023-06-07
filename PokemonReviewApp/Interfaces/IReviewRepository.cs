aaa6ea8ad336652464b2d8d122a0fbde2e84169a
velopace <velopace@outlook.com>
2023-06-07 08:24:10 -0400

    update review

--- PokemonReviewApp/Interfaces/IReviewRepository.cs
+++ PokemonReviewApp/Interfaces/IReviewRepository.cs
@@ -10,6 +10,7 @@ namespace PokemonReviewApp.Interfaces
         ICollection<Review> GetReviewsOfAPokemon(int pokeId);
         bool ReviewExists(int reviewId);
         bool CreateReview(Review review);
+        bool UpdateReview(Review review);
         bool Save();
     }
 }
